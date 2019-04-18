5796833754c56922721d599049666c425cbc8750
Christopher Sinlasath <csinlasath@live.com>
2019-04-17 21:27:05 -0500

    Updates

---
+++ precache-manifest.b4c2a6cc69886952daecce4a5852c09a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4c359075c5ae3fb0ae3",
+    "url": "/clicky-game/static/css/main.1f992b6a.chunk.css"
+  },
+  {
+    "revision": "f4c359075c5ae3fb0ae3",
+    "url": "/clicky-game/static/js/main.d7a176f3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "da703b4bfc926ffe484c",
+    "url": "/clicky-game/static/js/2.ebd7e023.chunk.js"
+  },
+  {
+    "revision": "d80bd5b7f94d7a1f36189878d22ce11e",
+    "url": "/clicky-game/index.html"
+  }
+];
